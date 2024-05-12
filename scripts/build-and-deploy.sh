5313f3973ec29b37f80c0b0526382dae16f5c7f2
Andrew Wobeck <dwobeck@gmail.com>
2024-05-12 17:19:00 -0500

    Add a build and deploy script

---
+++ scripts/build-and-deploy.sh
@@ -0,0 +1,19 @@
+﻿#!/bin/bash
+#check if TAG and PORT are set
+if [ -z "$TAG" ]; then
+    echo "Setting TAG to default value"
+    TAG="pokerogue"
+fi
+
+if [ -z "$PORT" ]; then
+    echo "Setting PORT to default value"
+    PORT=7678
+fi
+
+docker stop $(docker ps -a -q --filter ancestor=$TAG --format="{{.ID}}") 2> /dev/null
+
+cd $(git rev-parse --show-toplevel)
+
+docker build -t $TAG .
+
+docker run --restart=always -d -p $PORT:80 $TAG
